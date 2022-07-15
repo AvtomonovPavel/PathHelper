9780eac2deae834cf72797174e17bc84250cc8b1
Zaichenko <zaichenko.oa@cet-mipt.ru>
2022-07-15 15:33:02 +0300

    make snake cool again

--- Pathfinder.cpp
+++ Pathfinder.cpp
@@ -11,8 +11,8 @@ int main(int argc, char* argv[]) {
 	std::vector<std::vector<int>> block = {{0, 1, 2, 3, 4}, {32, 33, 34, 35, 36}, {64, 65, 66, 67, 68}};
 	Block b(block, xps);
 
-	Snake s(b, HORISONTAL);
-	auto snake_p = s.SnakeRun(68);
+	Snake s(b, VERTICAL);
+	auto snake_p = s.SnakeRun(4);
 	for(auto p : snake_p) {
 		std::cout << p << " ";
 	}
--- Snake/Snake.cpp
+++ Snake/Snake.cpp
@@ -14,40 +14,34 @@ Snake::Snake(const Block& block, SnakeType type) :
 std::vector<int> Snake::SnakeRun(int start_point) {
 	std::vector<int> snake_path;
 
-	if(block_.IsCorner(start_point)) {
+	if(block_.IsCorner(start_point) || type_ == NONETYPE) {
 		std::cout << "corner ok\n";
 		auto point_type = block_.GetPointType(start_point);
-		int x_step = (point_type == DLCORNER || point_type == ULCORNER) ? 1 : -1;
-		int y_step = (point_type == DLCORNER || point_type == DRCORNER) ? 1 : -1;;
+
+		int main_step = (point_type == DLCORNER || point_type == DRCORNER && type_ == VERTICAL ||
+			point_type == ULCORNER && type_ == HORISONTAL) ? 1 : -1;
+		int side_step = (point_type == DLCORNER || point_type == DRCORNER && type_ == HORISONTAL ||
+			point_type == ULCORNER && type_ == VERTICAL) ? 1 : -1;
+
 		int x_start = (point_type == DLCORNER || point_type == ULCORNER) ? 0 : block_.Size().first - 1;
 		int y_start = (point_type == DLCORNER || point_type == DRCORNER) ? 0 : block_.Size().second - 1;
+		int main_start = (type_ == VERTICAL) ? y_start : x_start;
+		int side_start = (type_ == VERTICAL) ? x_start : y_start;
 
-		if(type_ == VERTICAL) {
-			std::cout << "vertical ok\n";
-			for(int x = x_start; x >= 0 && x < block_.Size().first; x += x_step) {
-				for(int y = y_start; y >= 0 && y < block_.Size().second; y += y_step) {
-					snake_path.push_back(block_.GetPoints()[x][y]);
-				}
-				y_start = block_.Size().second - 1 - y_start;
-				y_step *= -1;
+		int main_limit = (type_ == VERTICAL) ? block_.Size().second : block_.Size().first;
+		int side_limit = (type_ == VERTICAL) ? block_.Size().first : block_.Size().second;
+
+		for(int side_coord = side_start; side_coord >= 0 && side_coord < side_limit; side_coord += side_step) {
+			for(int main_coord = main_start; main_coord >= 0 && main_coord < main_limit; main_coord += main_step) {
+				snake_path.push_back(block_.GetPoints()[(type_ == VERTICAL) ? 
+					side_coord : main_coord][(type_ == VERTICAL) ? main_coord : side_coord]);
 			}
-		}
-		else if(type_ == HORISONTAL) {
-			std::cout << "horisontal ok\n";
-			for(int y = y_start; y >= 0 && y < block_.Size().second; y += y_step) {
-				for(int x = x_start; x >= 0 && x < block_.Size().first; x += x_step) {
-					snake_path.push_back(block_.GetPoints()[x][y]);
-				}
-				x_start = block_.Size().first - 1 - x_start;
-				x_step *= -1;
-			}
-		}
-		else {
-			std::cout << "snake error1\n";
+			main_start = main_limit - 1 - main_start;
+			main_step *= -1;
 		}
 	}
 	else {
-		std::cout << "snake error2\n";
+		std::cout << "snake error\n";
 	}
 	return snake_path;
 }
